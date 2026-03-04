ee0ec7820ae6bd95b717972fade3e16198dd8eb2
coderbharatpathak <coderbharatpathak>
2026-03-04 09:31:21 +0530

    Fix Vite config & package.json for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/"
+  base: '/bharat-portfolio/', // ✅ Ye line add karo
 })
